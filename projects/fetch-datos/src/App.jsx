25889b3d6328d53a44a859e1af3d7a40c4717e9f
IagoPorto <iagopm8@gmail.com>
2025-01-19 08:48:24 +0100

    Adding fetching de datos in cat api project

--- projects/fetch-datos/src/App.jsx
+++ projects/fetch-datos/src/App.jsx
@@ -3,20 +3,47 @@ import { useEffect } from 'react'
 import './App.css'
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
 //const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=58&color=red&json=true`
 
 function App() {
-  const [fact, setFact] = useState('lorem ipsum cat fact whatever')
+  const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
+  const [factError, setFactError] = useState()
+  
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then(res => res.json())
-      .then(data => setFact(data.fact))
+      .then(res => {
+        if(!res.ok){
+          setFactError('No se ha podido recuperar la información')
+        }
+        return res.json()})
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+      })
   }, [])
+
+  useEffect(() => {
+    if(!fact) return
+    // const firstWord = fact.split(' ')[0]
+    // const firstWord = fact.split(' ', 3).join(' ')
+    const threeFirstsWord = fact.split(' ').slice(0, 3).join(' ')
+    
+    fetch(`https://cataas.com/cat/says/${threeFirstsWord}?size=50&color=red&json=true`)
+      .then(res => res.json())
+      .then(response => {
+        const { url } = response
+        console.log(url)
+        setImageUrl(url)
+      })
+  }, [fact])
 
   return (
     <main>
       <h1>App de gatitos</h1>
-      <p>{fact}</p>
+      {fact && <p>{fact}</p>}
+      {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the first three words for ${fact}`} />}
     </main>
   )
 }
